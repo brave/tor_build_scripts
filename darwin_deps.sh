b26edfa71414d5dc9a9654656dc0189bb0b5adc1
Pranjal <pranjal@brave.com>
2018-05-14 21:17:42 -0700

    Changes per comments

--- darwin_deps.sh
+++ darwin_deps.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install gnupg
+curl -o gnupg-2.2.7.tar.bz2 https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.7.tar.bz2
+curl -o gnupg-2.2.7.tar.bz2.sig https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.7.tar.bz2.sig
+echo "e222cda63409a86992369df8976f6c7511e10ea0  gnupg-2.2.7.tar.bz2" | shasum -a 256 -c -
+tar -xvjf gnupg-2.2.7.tar.bz2
+cd gnupg-2.2.7/
+./configure
+make && make check && sudo make install
