3fbbe05c49de530e69eb111dc014c69d38da4e12
Pranjal <pranjal@brave.com>
2018-05-17 12:21:13 -0700

    1. Statically linking openssl | libevent in tor for mac 2. Correct binary naming for windows and linux 3. Move hashes/keys to env.sh

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 set -eu
 
 docker rm -fv tor-brave || true
-docker build -t tor-brave -f Dockerfile-linux --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION .
+docker build -t tor-brave -f Dockerfile-linux --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION --build-arg zlib_hash=$ZLIB_HASH --build-arg libevent_hash=$LIBEVENT_HASH --build-arg libevent_key=$LIBEVENT_KEY --build-arg openssl_hash=$OPENSSL_HASH --build-arg tor_hash=$TOR_HASH --build-arg tor_key=$TOR_KEY .
 docker run --name tor-brave -d tor-brave
-docker cp tor-brave:/tor-$TOR_VERSION/src/or/tor tor-linux-$TOR_VERSION-brave-$BRAVE_TOR_VERSION
+docker cp tor-brave:/tor-$TOR_VERSION/src/or/tor tor-$TOR_VERSION-linux-brave-$BRAVE_TOR_VERSION
--- build_win32.sh
+++ build_win32.sh
@@ -4,14 +4,14 @@ set -eu
 rm -rf Tor/
 rm -rf Data/
 
-curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-0.3.2.10.zip" -o tor-win32-0.3.2.10.zip
-curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-0.3.2.10.zip.asc" -o tor-win32-0.3.2.10.zip.asc
+curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_VERSION.zip" -o tor-win32-$TOR_VERSION.zip
+curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_VERSION.zip.asc" -o tor-win32-$TOR_VERSION.zip.asc
 
-gpg --keyserver pgp.mit.edu/ --recv D1483FA6C3C07136
-gpg tor-win32-0.3.2.10.zip.asc
+gpg --keyserver pool.sks-keyservers.net/ --recv-keys $TOR_KEY
+gpg tor-win32-$TOR_VERSION.zip.asc
 
-unzip tor-win32-0.3.2.10.zip
+unzip tor-win32-$TOR_VERION.zip
 cd Tor
-zip -r tor-win32-$TOR_VERSION-brave-$BRAVE_TOR_VERSION.zip .
+zip -r tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
 cd .. 
-mv Tor/tor-win32-$TOR_VERSION-brave-$BRAVE_TOR_VERSION.zip .
+mv Tor/tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
