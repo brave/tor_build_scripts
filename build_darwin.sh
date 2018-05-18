11eea374ebb5ecad520df6df01671dd0568ac0a3
Pranjal Jumde <pranjal.jumde@gmail.com>
2018-05-18 15:28:34 -0700

    Updating build scripts for tor
    1. Statically linking openssl | libevent in tor for mac (#5)
    2. Correct binary naming for windows and linux
    3. Move hashes/keys to env.sh

--- build_darwin.sh
+++ build_darwin.sh
@@ -1,15 +1,48 @@
 #!/bin/sh
 set -eu
 
-curl -fsSL "https://www.torproject.org/dist/tor-0.3.2.10.tar.gz" -o tor-0.3.2.10.tar.gz
-curl -fsSL "https://www.torproject.org/dist/tor-0.3.2.10.tar.gz.asc" -o tor-0.3.2.10.tar.gz.asc
+curl -fsSL "https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz" -o openssl-$OPENSSL_VERSION.tar.gz && \
+echo "$OPENSSL_HASH  openssl-$OPENSSL_VERSION.tar.gz" | shasum -a 256 -c - && \
+tar -xvzf openssl-$OPENSSL_VERSION.tar.gz && \
+cd openssl-$OPENSSL_VERSION && \
+./config --prefix=$PWD/install no-shared no-dso && \
+make
 
-gpg --keyserver pgp.mit.edu/ --recv FE43009C4607B1FB
-gpg tor-0.3.2.10.tar.gz.asc
+cd ..
 
-tar -xvzf tor-0.3.2.10.tar.gz
-cd tor-0.3.2.10
-./configure && make && make install
+curl -fsSL "https://github.com/libevent/libevent/releases/download/release-$LIBEVENT_VERSION/libevent-$LIBEVENT_VERSION.tar.gz" -o libevent-$LIBEVENT_VERSION.tar.gz && \
+curl -fsSL "https://github.com/libevent/libevent/releases/download/release-$LIBEVENT_VERSION/libevent-$LIBEVENT_VERSION.tar.gz.asc" -o libevent-$LIBEVENT_VERSION.tar.gz.asc && \
+
+gpg --keyserver pool.sks-keyservers.net/ --recv-keys $LIBEVENT_KEY && \
+gpg libevent-$LIBEVENT_VERSION.tar.gz.asc && \
+echo "$LIBEVENT_HASH  libevent-$LIBEVENT_VERSION.tar.gz" | shasum -a 256 -c - && \
+tar -zxvf libevent-$LIBEVENT_VERSION.tar.gz && \
+cd libevent-$LIBEVENT_VERSION && \
+./configure \
+            LDFLAGS="-L$PWD/../openssl-$OPENSSL_VERSION" \
+            CPPFLAGS="-I$PWD/../openssl-$OPENSSL_VERSION/include" \
+            --prefix=$PWD/install \
+            --disable-shared \
+            --enable-static \
+            --with-pic && \
+make && make check && make install
+
+cd ..
+
+curl -fsSL "https://www.torproject.org/dist/tor-$TOR_VERSION.tar.gz" -o tor-$TOR_VERSION.tar.gz
+curl -fsSL "https://www.torproject.org/dist/tor-$TOR_VERSION.tar.gz.asc" -o tor-$TOR_VERSION.tar.gz.asc
+
+gpg --keyserver pool.sks-keyservers.net/ --recv-keys $TOR_KEY
+gpg tor-$TOR_VERSION.tar.gz.asc
+echo "$TOR_HASH  tor-$TOR_VERSION.tar.gz" | shasum -a 256 -c - && \
+tar -xvzf tor-$TOR_VERSION.tar.gz
+cd tor-$TOR_VERSION && \
+./configure --enable-static-libevent \
+            --enable-static-openssl  \
+            --with-libevent-dir=$PWD/../libevent-$LIBEVENT_VERSION/install \
+            --with-openssl-dir=$PWD/../openssl-$OPENSSL_VERSION/install \
+            --disable-asciidoc && \
+make && make check
 cd ..
 
 cp tor-0.3.2.10/src/or/tor tor-darwin-$TOR_VERSION-brave-$BRAVE_TOR_VERSION
