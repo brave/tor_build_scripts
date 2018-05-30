e4ba6fac916d910edec0c4515c45ee918fde7162
Pranjal Jumde <pranjal.jumde@gmail.com>
2018-05-30 15:27:08 -0700

    Fixes: #10 - Update to latest Tor stable binary
    1. Updating tor binaries to 0.3.3.6
    2. Move keyserver to env.sh
    3. The expert bundle for win32 has not been updated yet, so the version for windows remains the same.

--- build_win32.sh
+++ build_win32.sh
@@ -4,14 +4,14 @@ set -eu
 rm -rf Tor/
 rm -rf Data/
 
-curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_VERSION.zip" -o tor-win32-$TOR_VERSION.zip
-curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_VERSION.zip.asc" -o tor-win32-$TOR_VERSION.zip.asc
+curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_WIN32_VERSION.zip" -o tor-win32-$TOR_WIN32_VERSION.zip
+curl -fsSL "https://dist.torproject.org/torbrowser/7.5.4/tor-win32-$TOR_WIN32_VERSION.zip.asc" -o tor-win32-$TOR_WIN32_VERSION.zip.asc
 
-gpg --keyserver pool.sks-keyservers.net/ --recv-keys $TOR_KEY
-gpg tor-win32-$TOR_VERSION.zip.asc
+gpg --keyserver "$KEYSERVER" --recv-keys $TOR_KEY
+gpg tor-win32-$TOR_WIN32_VERSION.zip.asc
 
-unzip tor-win32-$TOR_VERION.zip
+unzip tor-win32-$TOR_WIN32_VERSION.zip
 cd Tor
-zip -r tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
+zip -r tor-$TOR_WIN32_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
 cd .. 
-mv Tor/tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
+mv Tor/tor-$TOR_WIN32_VERSION-win32-brave-$BRAVE_TOR_VERSION.zip .
