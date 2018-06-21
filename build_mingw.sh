9119460ca0e4ca2bb93f55738ee662abfc454b38
Taylor R Campbell <taylor@brave.com>
2018-06-21 18:36:47 +0000

    Executable has .exe in the name because this is Windows.

--- build_mingw.sh
+++ build_mingw.sh
@@ -4,4 +4,4 @@ set -eu
 docker rm -fv tor-brave-mingw || true
 docker build -t tor-brave-mingw -f Dockerfile-mingw --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION --build-arg zlib_hash=$ZLIB_HASH --build-arg libevent_hash=$LIBEVENT_HASH --build-arg libevent_key=$LIBEVENT_KEY --build-arg openssl_hash=$OPENSSL_HASH --build-arg tor_hash=$TOR_HASH --build-arg tor_key=$TOR_KEY --build-arg keyserver="$KEYSERVER" --build-arg jobs=12 .
 docker run --name tor-brave-mingw -d tor-brave-mingw
-docker cp tor-brave-mingw:/tor-$TOR_VERSION/src/or/tor tor-mingw-$TOR_VERSION-mingw-brave-$BRAVE_TOR_VERSION
+docker cp tor-brave-mingw:/tor-$TOR_VERSION/src/or/tor.exe tor-mingw-$TOR_VERSION-mingw-brave-$BRAVE_TOR_VERSION.exe
