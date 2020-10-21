9b0a1d54b61e39a3e195f7c29a407a251c7583b1
Pernat1y <1ns@pm.me>
2020-10-21 23:56:23 +0300

    Updated Tor, libevent, OpenSSL
    Updated:
    Tor 0.3.5.11 -> 0.4.4.5
    libevent 2.1.9-beta -> 2.1.11-stable
    OpenSSL 1.0.2q -> 1.0.2u
    + small refactoring for build scripts.

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,16 @@
 set -eu
 
 docker rm -fv tor-brave || true
-docker build -t tor-brave -f Dockerfile-linux --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION --build-arg zlib_hash=$ZLIB_HASH --build-arg libevent_hash=$LIBEVENT_HASH --build-arg openssl_hash=$OPENSSL_HASH --build-arg tor_hash=$TOR_HASH ${1+"$@"} .
+docker build -t tor-brave -f Dockerfile-linux \
+    --build-arg tor_version=$TOR_VERSION \
+    --build-arg zlib_version=$ZLIB_VERSION \
+    --build-arg libevent_version=$LIBEVENT_VERSION \
+    --build-arg openssl_version=$OPENSSL_VERSION \
+    --build-arg zlib_hash=$ZLIB_HASH \
+    --build-arg libevent_hash=$LIBEVENT_HASH \
+    --build-arg openssl_hash=$OPENSSL_HASH \
+    --build-arg tor_hash=$TOR_HASH \
+    ${1+"$@"} .
 docker run --name tor-brave -d tor-brave
 docker cp tor-brave:/tor-$TOR_VERSION/install/bin/tor tor-$TOR_VERSION-linux-brave-$BRAVE_TOR_VERSION
 
--- build_mingw.sh
+++ build_mingw.sh
@@ -2,6 +2,15 @@
 set -eu
 
 docker rm -fv tor-brave-mingw || true
-docker build -t tor-brave-mingw -f Dockerfile-mingw --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION --build-arg zlib_hash=$ZLIB_HASH --build-arg libevent_hash=$LIBEVENT_HASH --build-arg openssl_hash=$OPENSSL_HASH --build-arg tor_hash=$TOR_HASH ${1+"$@"} .
+docker build -t tor-brave-mingw -f Dockerfile-mingw \
+    --build-arg tor_version=$TOR_VERSION \
+    --build-arg zlib_version=$ZLIB_VERSION \
+    --build-arg libevent_version=$LIBEVENT_VERSION \
+    --build-arg openssl_version=$OPENSSL_VERSION \
+    --build-arg zlib_hash=$ZLIB_HASH \
+    --build-arg libevent_hash=$LIBEVENT_HASH \
+    --build-arg openssl_hash=$OPENSSL_HASH \
+    --build-arg tor_hash=$TOR_HASH \
+    ${1+"$@"} .
 docker run --name tor-brave-mingw -d tor-brave-mingw
 docker cp tor-brave-mingw:/tor-$TOR_VERSION/install/bin/tor.exe tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.exe
