b5c7b58ff0ffbde130cc749e6d67d714d50872c6
Pernat1y <1ns@pm.me>
2020-11-20 20:11:06 +0200

    Fix #52: Update tor to 0.4.4.6
    Updated:
    
    Tor 0.3.5.11 -> 0.4.4.6
    libevent 2.1.9-beta -> 2.1.11-stable
    OpenSSL 1.0.2q -> 1.0.2u

--- build_mingw.sh
+++ build_mingw.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -eu
 
-docker rm -fv tor-brave-mingw || true
-docker build -t tor-brave-mingw -f Dockerfile-mingw --build-arg tor_version=$TOR_VERSION --build-arg zlib_version=$ZLIB_VERSION --build-arg libevent_version=$LIBEVENT_VERSION --build-arg openssl_version=$OPENSSL_VERSION --build-arg zlib_hash=$ZLIB_HASH --build-arg libevent_hash=$LIBEVENT_HASH --build-arg openssl_hash=$OPENSSL_HASH --build-arg tor_hash=$TOR_HASH ${1+"$@"} .
+docker rm -fv tor-brave-mingw 2>/dev/null || true
+docker build -t tor-brave-mingw -f Dockerfile-mingw \
+    --build-arg tor_version=$TOR_VERSION \
+    --build-arg zlib_version=$ZLIB_VERSION \
+    --build-arg libevent_version=$LIBEVENT_VERSION \
+    --build-arg openssl_version=$OPENSSL_VERSION \
+    --build-arg zlib_hash=$ZLIB_HASH \
+    --build-arg libevent_hash=$LIBEVENT_HASH \
+    --build-arg openssl_hash=$OPENSSL_HASH \
+    --build-arg tor_hash=$TOR_HASH \
+    ${1+"$@"} .
 docker run --name tor-brave-mingw -d tor-brave-mingw
 docker cp tor-brave-mingw:/tor-$TOR_VERSION/install/bin/tor.exe tor-$TOR_VERSION-win32-brave-$BRAVE_TOR_VERSION.exe
