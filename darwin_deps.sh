75588608e29ec68e7412590ec2a74dd6503fb3c7
Francois Marier <francois@brave.com>
2022-04-06 20:36:06 -0700

    Upgrade the version of GPG on Mac.
    Verified on a Linux machine using:
    
    ```
    $ curl --tlsv1.2 --proto '=https' -fsSL https://gnupg.org/signature_key.asc | gpg --import
    gpg: key BCEF7E294B092E28: 1 signature not checked due to a missing key
    gpg: key BCEF7E294B092E28: public key "Andre Heinecke (Release Signing Key)" imported
    gpg: key 528897B826403ADA: 4 signatures not checked due to missing keys
    gpg: key 528897B826403ADA: public key "Werner Koch (dist signing 2020)" imported
    gpg: key E98E9B2D19C6C8BD: 2 signatures not checked due to missing keys
    gpg: key E98E9B2D19C6C8BD: public key "Niibe Yutaka (GnuPG Release Key)" imported
    gpg: key 549E695E905BA208: 1 signature not checked due to a missing key
    gpg: key 549E695E905BA208: public key "GnuPG.com (Release Signing Key 2021)" imported
    gpg: Total number processed: 4
    gpg:               imported: 4
    gpg: marginals needed: 3  completes needed: 1  trust model: pgp
    gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
    gpg: next trustdb check due at 2025-01-14
    
    $ gpg --verify gnupg-2.3.4.tar.bz2.sig gnupg-2.3.4.tar.bz2
    gpg: Signature made Mon 20 Dec 2021 01:52:45 PM PST
    gpg:                using EDDSA key 6DAA6E64A76D2840571B4902528897B826403ADA
    gpg: Good signature from "Werner Koch (dist signing 2020)" [unknown]
    gpg: WARNING: This key is not certified with a trusted signature!
    gpg:          There is no indication that the signature belongs to the owner.
    Primary key fingerprint: 6DAA 6E64 A76D 2840 571B  4902 5288 97B8 2640 3ADA
    gpg: Signature made Mon 20 Dec 2021 10:20:39 PM PST
    gpg:                using EDDSA key AC8E115BF73E2D8D47FA9908E98E9B2D19C6C8BD
    gpg: Good signature from "Niibe Yutaka (GnuPG Release Key)" [unknown]
    gpg: WARNING: This key is not certified with a trusted signature!
    gpg:          There is no indication that the signature belongs to the owner.
    Primary key fingerprint: AC8E 115B F73E 2D8D 47FA  9908 E98E 9B2D 19C6 C8BD
    ```
    
    https://www.gnupg.org/download/integrity_check.html
    https://www.gnupg.org/signature_key.html

--- darwin_deps.sh
+++ darwin_deps.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -eu
 
-curl -o gnupg-2.2.7.tar.bz2 https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.7.tar.bz2
-curl -o gnupg-2.2.7.tar.bz2.sig https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.7.tar.bz2.sig
-echo "e222cda63409a86992369df8976f6c7511e10ea0  gnupg-2.2.7.tar.bz2" | shasum -a 256 -c -
-tar -xvjf gnupg-2.2.7.tar.bz2
-cd gnupg-2.2.7/
+GPG_VERSION=2.3.4
+GPG_HASH=f3468ecafb1d7f9ad7b51fd1db7aebf17ceb89d2efa8a05cf2f39b4d405402ae
+
+curl --proto '=https' --tlsv1.2 -fsSL -o "gnupg-$GPG_VERSION.tar.bz2" "https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-$GPG_VERSION.tar.bz2"
+echo "$GPG_HASH  gnupg-$GPG_VERSION.tar.bz2" | shasum -a 256 -c -
+tar -xjf "gnupg-$GPG_VERSION.tar.bz2"
+cd "gnupg-$GPG_VERSION/"
 ./configure
 make && make check && sudo make install
