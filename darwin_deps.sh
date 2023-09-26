18a66377b8b309dc0c562327bbb84326ba38cf92
Francois Marier <francois@brave.com>
2023-09-26 11:56:21 -0700

    Upgrade to gnupg 2.4.3
    https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.3.tar.bz2

--- darwin_deps.sh
+++ darwin_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 
-GPG_VERSION=2.3.4
-GPG_HASH=f3468ecafb1d7f9ad7b51fd1db7aebf17ceb89d2efa8a05cf2f39b4d405402ae
+GPG_VERSION=2.4.3
+GPG_HASH=a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d
 
 curl --proto '=https' --tlsv1.2 -fsSL -o "gnupg-$GPG_VERSION.tar.bz2" "https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-$GPG_VERSION.tar.bz2"
 echo "$GPG_HASH  gnupg-$GPG_VERSION.tar.bz2" | shasum -a 256 -c -
