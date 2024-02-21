454d7c9cfaf985019294cb8d937dab51cbc8a4c5
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2024-02-21 14:32:30 +0000

    Defaults to docker

--- env.sh
+++ env.sh
@@ -16,4 +16,4 @@ export LIBEVENT_HASH=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0
 export OPENSSL_HASH=cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8
 export TOR_HASH=59bb7d8890f6131b4ce5344f3dcea5deb2182b7f4f10ff0cb4e4d81f11b2cf65
 
-export DOCKER="$(if command -v podman >/dev/null 2>&1; then echo podman; else echo docker; fi)"
+export DOCKER="$(command -v docker || command -v podman)"
