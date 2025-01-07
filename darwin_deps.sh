2eedf833510876d7db3420341f278a431168d34f
Francois Marier <francois@brave.com>
2025-01-06 21:29:50 -0800

    Upgrade gnupg dependency on macOS to 2.4.7
    https://lists.gnupg.org/pipermail/gnupg-announce/2024q4/000487.html

--- darwin_deps.sh
+++ darwin_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 
-GPG_VERSION=2.4.3
-GPG_HASH=a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d
+GPG_VERSION=2.4.7
+GPG_HASH=7b24706e4da7e0e3b06ca068231027401f238102c41c909631349dcc3b85eb46
 
 curl --proto '=https' --tlsv1.2 -fsSL -o "gnupg-$GPG_VERSION.tar.bz2" "https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-$GPG_VERSION.tar.bz2"
 echo "$GPG_HASH  gnupg-$GPG_VERSION.tar.bz2" | shasum -a 256 -c -
