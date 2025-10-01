188251d0c37a4991f21847acd8c5dcd9c1047df3
Francois Marier <francois@brave.com>
2025-10-01 16:06:27 -0700

    Normalize indentation

--- build_darwin_arm64.sh
+++ build_darwin_arm64.sh
@@ -27,34 +27,34 @@ cd ../../
 tar -xvzf "openssl-$OPENSSL_VERSION.tar.gz" -C arm64
 cd "arm64/openssl-$OPENSSL_VERSION"
 ./Configure --prefix="$PWD/root" \
-            darwin64-arm64-cc \
-            no-apps \
-            no-cmp \
-            no-cms \
-            no-comp \
-            no-ct \
-            no-dgram \
-            no-docs \
-            no-dso \
-            no-ec2m \
-            no-engine \
-            no-http \
-            no-legacy \
-            no-module \
-            no-nextprotoneg \
-            no-ocsp \
-            no-padlockeng \
-            no-psk \
-            no-quic \
-            no-rfc3779 \
-            no-shared \
-            no-srp \
-            no-srtp \
-            no-ssl-trace \
-            no-static-engine \
-            no-ts \
-            no-ui-console \
-            no-uplink
+  darwin64-arm64-cc \
+  no-apps \
+  no-cmp \
+  no-cms \
+  no-comp \
+  no-ct \
+  no-dgram \
+  no-docs \
+  no-dso \
+  no-ec2m \
+  no-engine \
+  no-http \
+  no-legacy \
+  no-module \
+  no-nextprotoneg \
+  no-ocsp \
+  no-padlockeng \
+  no-psk \
+  no-quic \
+  no-rfc3779 \
+  no-shared \
+  no-srp \
+  no-srtp \
+  no-ssl-trace \
+  no-static-engine \
+  no-ts \
+  no-ui-console \
+  no-uplink
 make ${jobs:+-j${jobs}} && make test && make install
 cd ../../
 
@@ -66,15 +66,15 @@ tar -zxvf "libevent-$LIBEVENT_VERSION.tar.gz" -C arm64
 cd "arm64/libevent-$LIBEVENT_VERSION"
 patch -p0 < ../../patch/libevent/regress.c.patch
 ./configure \
-	    LDFLAGS="-L$PWD/../openssl-$OPENSSL_VERSION/root/lib --target=arm64-apple-macos11" \
-	    CPPFLAGS="-I$PWD/../openssl-$OPENSSL_VERSION/include --target=arm64-apple-macos11" \
-	    --prefix="$PWD/install" \
-	    --disable-openssl \
-	    --disable-shared \
-	    --enable-static \
-            --host=arm-apple-darwin \
-	    --disable-clock-gettime \
-	    --with-pic
+  LDFLAGS="-L$PWD/../openssl-$OPENSSL_VERSION/root/lib --target=arm64-apple-macos11" \
+  CPPFLAGS="-I$PWD/../openssl-$OPENSSL_VERSION/include --target=arm64-apple-macos11" \
+  --prefix="$PWD/install" \
+  --disable-openssl \
+  --disable-shared \
+  --enable-static \
+  --host=arm-apple-darwin \
+  --disable-clock-gettime \
+  --with-pic
 make ${jobs:+-j${jobs}} && make ${jobs:+-j${jobs}} check && make install
 cd ../../
 
@@ -82,25 +82,25 @@ tar -xvzf "tor-$TOR_VERSION.tar.gz" -C arm64
 cd "arm64/tor-$TOR_VERSION"
 patch -p0 < ../../patch/tor/test_slow.c.patch
 ./configure \
-	    LDFLAGS="--target=arm64-apple-macos11 -L$XCODE_LIB" \
-	    CPPFLAGS="--target=arm64-apple-macos11 -I$XCODE_INCLUDE" \
-	    --prefix="$PWD/root" \
-	    --enable-static-libevent \
-	    --enable-static-openssl  \
-	    --enable-static-zlib  \
-	    --with-libevent-dir="$PWD/../libevent-$LIBEVENT_VERSION/install" \
-	    --with-openssl-dir="$PWD/../openssl-$OPENSSL_VERSION/root" \
-	    --with-zlib-dir="$PWD/../zlib-$ZLIB_VERSION/root" \
-	    --disable-asciidoc \
-	    --disable-html-manual \
-	    --disable-lzma \
-	    --disable-manpage \
-            --disable-zstd \
-            --disable-module-relay \
-            --disable-module-dirauth \
-            --host=arm-apple-darwin \
-            --disable-tool-name-check \
-	    ac_cv_func_getentropy=no \
-	    ac_cv_func_clock_gettime=no
+  LDFLAGS="--target=arm64-apple-macos11 -L$XCODE_LIB" \
+  CPPFLAGS="--target=arm64-apple-macos11 -I$XCODE_INCLUDE" \
+  --prefix="$PWD/root" \
+  --enable-static-libevent \
+  --enable-static-openssl  \
+  --enable-static-zlib  \
+  --with-libevent-dir="$PWD/../libevent-$LIBEVENT_VERSION/install" \
+  --with-openssl-dir="$PWD/../openssl-$OPENSSL_VERSION/root" \
+  --with-zlib-dir="$PWD/../zlib-$ZLIB_VERSION/root" \
+  --disable-asciidoc \
+  --disable-html-manual \
+  --disable-lzma \
+  --disable-manpage \
+  --disable-zstd \
+  --disable-module-relay \
+  --disable-module-dirauth \
+  --host=arm-apple-darwin \
+  --disable-tool-name-check \
+  ac_cv_func_getentropy=no \
+  ac_cv_func_clock_gettime=no
 make ${jobs:+-j${jobs}} && make ${jobs:+-j${jobs}} check && make install
 cd ../../
