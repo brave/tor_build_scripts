be8a7cf4215a677af3e53350d9f93c6a668d8e6d
brave-builds <brave-builds+devops@brave.com>
2026-06-26 19:04:36 +0000

    tor update

--- env.sh
+++ env.sh
@@ -6,15 +6,15 @@ export MACOSX_DEPLOYMENT_TARGET=10.15
 # Reset version number to zero everytime TOR_VERSION changes.
 export BRAVE_TOR_VERSION="0"
 
-export TOR_VERSION="0.4.9.9"
+export TOR_VERSION="0.4.9.11"
 
 export ZLIB_VERSION="1.3.2"
 export LIBEVENT_VERSION="2.1.12-stable"
-export OPENSSL_VERSION="4.0.0"
+export OPENSSL_VERSION="4.0.1"
 
 export ZLIB_HASH="bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16"
 export LIBEVENT_HASH=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
-export OPENSSL_HASH="c32cf49a959c4f345f9606982dd36e7d28f7c58b19c2e25d75624d2b3d2f79ac"
-export TOR_HASH="bd75ba7fd68f607c7806fcf70156a300aa926e9ad69a5e56a8e6414f5227e833"
+export OPENSSL_HASH="2db3f3a0d6ea4b59e1f094ace2c8cd536dffb87cdc39084c5afa1e6f7f37dd09"
+export TOR_HASH="2e6c1720118c812acf0079fd47cf91b6bfaba5d766c321c4d3d2a28d6a11a8ed"
 
 export DOCKER="$(command -v docker || command -v podman)"
